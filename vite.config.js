2cd04312e2547dd7f4a68b5d83dae0ae22644f69
umair-iqbal-61 <ui0886342@gmail.com>
2025-08-07 01:48:04 +0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/chef-stop/',
+  base: '/chef-stop',
   plugins: [react(),tailwindcss()],
 })
